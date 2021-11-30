0e6432c378bc686bf0c3fbdd662c5284959d6467
Luisa Bermeo <ferbermeo04@gmail.com>
2021-11-30 15:51:39 -0500

    implements controllers

---
+++ src/main/java/dao/ICRUD.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.util.List;
+
+
+public interface ICRUD<T> {
+    List<T> selectAll();
+
+    int deleteAll();
+
+    T select(T entity);
+
+    int insert(T entity);
+
+    int update(T entity);
+
+    int delete(T entity);
+}
